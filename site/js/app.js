b715cf5ca97554bdd7d30102efe9c14eb8a6bc36
NikolayMishker <70703208+NikolayMishker@users.noreply.github.com>
2022-05-29 19:53:27 +0300

    Add files via upload

---
+++ site/js/app.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    document.body.classList.add("loaded");
+
+    setTimeout(function() {
+        Draggable.create('.gallery', {
+            bounds: 'body',
+            inertia: true
+        })
+    }, 200)
+}
